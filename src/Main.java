31173df48a1e98b9d73e17a472f5fc9c9b2e60ce
Dainius <dainius.vanagas@codeacademy.lt>
2022-12-18 17:45:20 +0200

    second try

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
 public class Main {
 
     public static void main(String[] args) {
-        
+
         System.out.println("Hello world!");
 
         System.out.println("it's another try for GIT");
+
+        System.out.println("-----------------");
+
+        System.out.println("this program suppose to help learning");
+
+        System.out.println("----------------");
     }
 }
